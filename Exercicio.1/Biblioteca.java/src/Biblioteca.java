5787ef755965b9e9706467107dcf697ae1727f7a
Eduardo correa <Eduzeraaoficial@gmail.com>
2024-06-02 22:36:08 -0300

    Finalizado

---
+++ Exercicio.1/Biblioteca.java/src/Biblioteca.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+    private List<Livro> livros ;
+
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void listarLivros() {
+        for (Livro livro : livros) {
+            System.out.println(livro);
+        }
+    }
+}
